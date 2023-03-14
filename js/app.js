767d1a79979e9a6d689ddaa1dfc3edf02f005bf5
elizonRL <elizorodriguez16@gmail.com>
2023-03-14 10:55:27 -0400

    mensaje de error

--- js/app.js
+++ js/app.js
@@ -1,17 +1,19 @@
 // Iniciasion de variables y opjetos 
-const nickInput = document.getElementById("nick")
-const tamanoInput = document.getElementById("tamano")
-const formEntrada = document.getElementById("formEntrada")
-
+const nickInput = document.getElementById("nick");
+const tamanoInput = document.getElementById("tamano");
+const formEntrada = document.getElementById("formEntrada");
+const error = document.getElementById("error");
 //funciones 
 function  comprobarForm(event){
     if(nickInput.value.length==0){
         console.log("no tien nick");
         nickInput.focus();
+        error.innerText="¡El campo Nick No puede estar vacio!"
         event.preventDefault();
     }else if(tamanoInput.value=="0"){
         console.log("Agrege EL tamaño del juego");
         tamanoInput.focus();
+        error.innerText="¡Debe Selecionar un tamaño de juego!"
         event.preventDefault();
     }
 }
